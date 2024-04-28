1c27e3c8193e55f40d09beae486948348cb9c735
Luis Maldonado <luis.maldonado.1366@gmail.com>
2024-04-27 19:45:04 -0600

    Setters and getters

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import com.aluracursos.screenmatch.models.Movie;
+
 public class Main {
     public static void main(String[] args) {
         Movie deadpool = new Movie();
-        deadpool.name = "Deadpool";
-        deadpool.releaseDate = 2016;
-        deadpool.durationInMinutes = 108;
-        deadpool.membershipIncluded = true;
+        deadpool.setName("Deadpool");
+        deadpool.setReleaseDate(2016);
+        deadpool.setDurationInMinutes(108);
+        deadpool.setMembershipIncluded(true);
 
         deadpool.displayMovieSpecs();
         deadpool.rateMovie(7.8);
@@ -12,12 +14,10 @@ public class Main {
         System.out.printf("\nTotal ratings: %d", deadpool.getTotalRatings());
         System.out.printf("\nAverage rating: %.2f", deadpool.calculateMean());
 
-
-        Movie starWarsEp3 = new Movie();
-        starWarsEp3.name = "Star Wars Episode 3: Revenge of the Sith";
-        starWarsEp3.releaseDate = 2005;
-        starWarsEp3.durationInMinutes = 140;
-        starWarsEp3.membershipIncluded = false;
+//        starWarsEp3.name = "Star Wars Episode 3: Revenge of the Sith";
+//        starWarsEp3.releaseDate = 2005;
+//        starWarsEp3.durationInMinutes = 140;
+//        starWarsEp3.membershipIncluded = false;
 
 //        starWarsEp3.displayMovieSpecs();
     }
